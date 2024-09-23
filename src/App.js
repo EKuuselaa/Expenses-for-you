e26a3a59f50dbff7832a7a77d045d1ae0e5e8af5
EKuuselaa <esko.kuusela1@gmail.com>
2024-09-23 14:56:12 +0300

    Komponenttien järjestelyä, nuolifunktiot lisätty ja react import lisätty

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import Expenses from './components/Expenses';
 
 function App() {
 
   const expenses = [
     { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2022 , 7, 14) },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 1, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 8, 12) },
     { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2023, 1, 1) },
-    { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2022, 5, 12) },
+    { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2022, 1, 9) },
   ];
 
 
