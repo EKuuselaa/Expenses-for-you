d3f0a8c342bc8ddf490f257fdcca783ece851fde
EKuuselaa <esko.kuusela1@gmail.com>
2024-09-24 18:21:12 +0300

    tehtävä 8: lisätty painettava nappula joka tekee asioita

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from './UI/Card';
 import './ExpenseItem.css';
 
+
 const ExpenseItem = (props) => {
+
+    const [title, SetTitle] = useState(props.title);
+
+    const clickHandler = () => {
+            SetTitle('Updated!');
+            console.log(title);
+    };
 
     return (
         <Card className="expense-item">
@@ -12,6 +20,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
