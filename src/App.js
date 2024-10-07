da3a884d3cc5152d89fa2ba6725d435ba3288022
EKuuselaa <esko.kuusela1@gmail.com>
2024-10-07 14:09:46 +0300

    TEHTÄVÄ 12. Filter-komponentin luonti

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
 
