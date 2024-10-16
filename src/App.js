020fecadddc72a15baef4ecef64a6abd1c5a980d
EKuuselaa <esko.kuusela1@gmail.com>
2024-10-16 10:13:26 +0300

    tehtävä 16. ehtorakenne renderöinnissä ja parannettu kommentteja

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Expenses from './components/Expenses/Expenses';
 //importataan NewExpense ja Expenses komponentit
 const dummy_expenses = [
   { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2022 , 7, 14) },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 8, 12) },
-  { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2023, 1, 1) },
-  { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2022, 1, 9) },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 8, 12) },
+  { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2020, 1, 1) },
+  { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 1, 9) },
 ];
 //määritellään dummy_expenses taulukko, joka sisältää 4 objektia
 function App() {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,38 @@
+//importataan React ja useState
+//importataan ExpenseItem, Card, ExpensesFilter ja Expenses.css
 import React , { useState } from 'react';
-//importataan useState hookki
 import ExpenseItem from '../Expenses/ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 //importataan ExpenseItem, Card, ExpensesFilter ja Expenses.css
- 
+ //expenses komponentti
 const Expenses = (props) => {
-//Expenses komponentti
+//määritellään filterChangeHandler funktio, joka saa parametrinaan valitun vuoden ja asettaa sen filteredYear muuttujaan
     const [filteredYear, setFilteredYear] = useState('2022');
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
     console.log(props.item);
 //määritellään filterChangeHandler funktio, joka saa parametrinaan valitun vuoden ja asettaa sen filteredYear muuttujaan
+//määritellään filteredExpenses muuttuja, joka suodattaa props.items taulukosta vuoden mukaan
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
-//määritellään filteredExpenses muuttuja, joka suodattaa props.items taulukosta vuoden mukaan
+
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {filteredExpenses.map((expense) => (
+            {filteredExpenses.length === 0 ? (<p>No expenses found.</p>) : (
+            filteredExpenses.map((expense) => (
                 <ExpenseItem
                     key={expense.id}
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}
                 />
-            ))}
+            ))
+            )}
         </Card>
     );
 //palautetaan Card komponentti, joka sisältää ExpensesFilter komponentin ja map funktiolla käydään läpi filteredExpenses taulukko ja palautetaan ExpenseItem komponentti
