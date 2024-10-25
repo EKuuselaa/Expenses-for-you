e27414073a28d3c47d4495a8f5e387e79516d9df
EKuuselaa <esko.kuusela1@gmail.com>
2024-10-25 12:25:58 +0300

    Tehtävä 17 Ehtorakenne jatkoa, vaihtoehtoiset return lausekkeet

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,40 +1,30 @@
-//importataan React ja useState
-//importataan ExpenseItem, Card, ExpensesFilter ja Expenses.css
+
 import React , { useState } from 'react';
-import ExpenseItem from '../Expenses/ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
-//importataan ExpenseItem, Card, ExpensesFilter ja Expenses.css
- //expenses komponentti
+import ExpensesList from './ExpensesList';
+
 const Expenses = (props) => {
-//määritellään filterChangeHandler funktio, joka saa parametrinaan valitun vuoden ja asettaa sen filteredYear muuttujaan
-    const [filteredYear, setFilteredYear] = useState('2022');
+
+    const [filteredYear, setFilteredYear] = useState('2023');
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
-    }
-    console.log(props.item);
-//määritellään filterChangeHandler funktio, joka saa parametrinaan valitun vuoden ja asettaa sen filteredYear muuttujaan
-//määritellään filteredExpenses muuttuja, joka suodattaa props.items taulukosta vuoden mukaan
+    };
+    
+
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
     return (
-        <Card className='expenses'>
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {filteredExpenses.length === 0 ? (<p>No expenses found.</p>) : (
-            filteredExpenses.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))
-            )}
+        <Card className="expenses">
+            <ExpensesFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler}
+            />
+            <ExpensesList items={filteredExpenses} />
         </Card>
     );
-//palautetaan Card komponentti, joka sisältää ExpensesFilter komponentin ja map funktiolla käydään läpi filteredExpenses taulukko ja palautetaan ExpenseItem komponentti
-}
+};
 export default Expenses;
