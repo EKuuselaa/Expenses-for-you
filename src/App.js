84c57255c9b1f3d3dec15f9bbd69cceb44ed9f77
EKuuselaa <esko.kuusela1@gmail.com>
2024-10-28 08:57:15 +0200

    Käyty läpi componentit ja tarkistettu mahdolliset virheet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
-//importataan useState hookki
+
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
-//importataan NewExpense ja Expenses komponentit
+
 const dummy_expenses = [
   { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2022 , 7, 14) },
   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 8, 12) },
   { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2020, 1, 1) },
   { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 1, 9) },
 ];
-//määritellään dummy_expenses taulukko, joka sisältää 4 objektia
+
 function App() {
-//App komponentti
+
   const [expenses, setExpenses] = useState(dummy_expenses);
-//määritellään expenses muuttuja, joka saa arvokseen dummy_expenses taulukon ja setExpenses funktion
+
   const addExpenseHandler = expense =>  {
     setExpenses((preExpenses) => {
       return [expense, ...preExpenses]
     });
   };
-//määritellään addExpenseHandler funktio, joka saa parametrinaan expense olion
+
 
   return (
     <div>
@@ -30,5 +30,5 @@ function App() {
     </div>
   );
 }
-//palautetaan div elementti, joka sisältää NewExpense ja Expenses komponentit sekä p elementin, joka sisältää nimen
+
 export default App;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -27,8 +27,8 @@ const ExpenseForm = (props) => {
             amount: +enteredAmount,
             date: new Date(enteredDate)
 
-        }
-        console.log(expenseData);
+        };
+        props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
@@ -49,12 +49,17 @@ const ExpenseForm = (props) => {
                 type='number'
                 min='0.01'
                 step='0.01'
+                value={enteredAmount}
                 onChange={amountChangeHandler} 
             />
         </div>
         <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+            <input type='date'
+             min='2019-01-01'
+             max='2022-12-31'
+             onChange={dateChangeHandler} 
+             />
         </div>
     </div>
     <div className='new-expense__actions'>
