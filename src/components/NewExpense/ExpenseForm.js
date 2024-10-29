e3a8aff2740aa48757a77ec127bb2b3e7ba365dc
EKuuselaa <esko.kuusela1@gmail.com>
2024-10-29 10:56:37 +0200

    nostettu kalenterin maximi vuotta 2022 --> 2024

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -57,7 +57,7 @@ const ExpenseForm = (props) => {
             <label>Date</label>
             <input type='date'
              min='2019-01-01'
-             max='2022-12-31'
+             max='2024-12-31'
              onChange={dateChangeHandler} 
              />
         </div>
